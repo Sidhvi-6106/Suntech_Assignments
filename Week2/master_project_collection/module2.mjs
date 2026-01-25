0497bdb2d286f884e51d4935e7f495e0b78f4dd6
Sidhvi-6106 <akulasidhvi@gmail.com>
2026-01-25 17:35:12 +0530

    module2.mjs

--- Week2/master_project_collection/module2.mjs
+++ Week2/master_project_collection/module2.mjs
@@ -4,5 +4,5 @@ export const sortedByPrice = [...courses].sort((a, b) => b.price - a.price);
 export const courseList = courses.map(c => ({ title: c.title, price: c.price }));
 export const totalValue = publishedCourses.reduce((sum, c) => sum + c.price, 0);
 export const newCourses = [...courses, { id: 104, title: "Next.js", price: 1999, published: true }];
-console.log("Total Published Value:", totalValue);
-console.log("Highest Price Course:", sortedByPrice[0].title);
+console.log(totalValue);
+console.log(sortedByPrice[0].title);
