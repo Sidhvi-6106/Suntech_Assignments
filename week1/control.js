3154fe89caeab8a98b1755a82b75ac1140eb83d1
Sidhvi-6106 <akulasidhvi@gmail.com>
2026-01-25 14:44:11 +0530

    control.js

--- week1/control.js
+++ week1/control.js
@@ -1,3 +1,4 @@
+//Smart Login Status Engine
 let isLoggedIn=true;
 let isProfileComplete=false;
 if(isLoggedIn==true && isProfileComplete==true){
@@ -9,3 +10,21 @@ if(isLoggedIn==true && isProfileComplete==false){
 if(isLoggedIn==false){
     console.log("Please Login")
 }
+
+//Course Price Tag Labeler
+let price=1299;
+if(price<500){
+    console.log("Budget Course")
+}
+if(price>=500 && price<=1000){
+    console.log("Standard course");
+}
+if(price>1000){
+    console.log("Premium Course")
+}
+
+//Enrollment Eligibility Checker
+let hasPaid=true;
+let hasCompletedBasics=false;
+let enrollMessage=(hasPaid && hasCompletedBasics) ? "Enroll Now":"Complete Requirements";
+console.log(enrollMessage)
